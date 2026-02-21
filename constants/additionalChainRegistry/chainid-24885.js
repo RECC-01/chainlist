34c75ef1baf0a3904309accce15ef4b74059fb2a
RECC-01 <proyectorecc@gmail.com>
2026-02-21 04:02:04 -0500

    Create chainid-24885.js
    This PR adds the RECCNETWORK to Chainlist.
    
    Network Name: RECCNETWORK
    Chain ID: 24885
    RPC URL: https://rpc.reccnetwork.com
    RPC URL: http://46.224.198.52:8545
    Currency Symbol: RTS
    Decimals: 18
    
    The network is an EVM-compatible blockchain built with Polygon Edge IBFT PoS.

---
+++ constants/additionalChainRegistry/chainid-24885.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "RECCNETWORK Mainnet",
+  chain: "RECC",
+  rpc: [
+    "https://rpc.reccnetwork.com",
+    "http://46.224.198.52:8545"
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "Regulatory Technical Standard",
+    symbol: "RTS",
+    decimals: 18
+  },
+  infoURL: "https://reccnetwork.com",
+  shortName: "RECC",
+  chainId: 24885,
+  networkId: 24885
+}
